8b4c3cdec576ea9f64bf56690d5bc0041146c372
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-06-02 12:01:21 -0300

    add: start and end point to result

--- random_walk_visual.py
+++ random_walk_visual.py
@@ -6,11 +6,21 @@ from random_walk import RandomWalk
 # Keep making new walks
 while True:
     # Make a random walk, and plot the points
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     point_numbers = list(range(rw.num_points))
-    plot.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plot.cm.Blues, s=15)
+    plot.scatter(rw.x_values, rw.y_values, c=point_numbers, 
+                 cmap=plot.cm.Blues, edgecolors='none', s=1)
+    
+    # Mark the first and last points
+    plot.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plot.scatter(rw.x_values[-1], rw.y_values[-1], c='red', 
+                 edgecolors='none', s=100)
+                 
+    # Remove the axes.
+    plot.axis('off')
+    
     plot.show()
     
     keep_running = input("Make another walk? (y/n): ")
