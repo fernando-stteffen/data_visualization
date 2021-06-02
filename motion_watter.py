e69ce8e3bc53dfbbcf3ad2839bbba176a3f7e310
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-06-02 19:07:15 -0300

    add: pollen walk in  motion_watter routine

--- motion_watter.py
+++ motion_watter.py
@@ -6,20 +6,19 @@ from random_walk import RandomWalk
 # Keep making new walks
 while True:
     # Make a random walk, and plot the points
-    rw = RandomWalk(50000)
+    rw = RandomWalk(5000)
     rw.fill_walk()
     
     # Set the size of the plotting window
     plot.figure(figsize=(20,12))
 
     point_numbers = list(range(rw.num_points))
-    plot.scatter(rw.x_values, rw.y_values, c=point_numbers, 
-                 cmap=plot.cm.Blues, edgecolors='none', s=1)
+    plot.plot(rw.x_values, rw.y_values, c="green", linewidth=5)
     
     # Mark the first and last points
-    plot.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plot.scatter(0, 0, c='red', edgecolors='none', s=100)
     plot.scatter(rw.x_values[-1], rw.y_values[-1], c='red', 
-                 edgecolors='none', s=100)
+                edgecolors='none', s=100)
                  
     # Remove the axes.
     plot.axis('off')
@@ -32,3 +31,4 @@ while True:
     
     if keep_running == 'n':
         break
+
