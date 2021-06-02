28427d42081779bcd034cce7b799cee4f73623a3
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-06-02 09:15:24 -0300

    add: more quare values and set de range to axis

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,17 +1,20 @@
 import matplotlib.pyplot as plot
 
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = list(range(1,1001))
+y_values = [x**2 for x in x_values]
 
 
-plot.scatter(x_values, y_values,  s=100)
+plot.scatter(x_values, y_values,  s=40)
 
 # Set chart title and label axes.
 plot.title("Square Numbers", fontsize=24)
 plot.xlabel("Value", fontsize=14)
 plot.ylabel("Square of Value", fontsize=14)
 
+
+# Set the range ofr each exis.
+plot.axis([0, 1100, 0, 1100000])
 
 # Set size of tick labels.
 plot.tick_params(axis='both', which='major', labelsize=14)
