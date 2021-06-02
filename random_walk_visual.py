9f324090733d450ba52b785a5b47d714390af363
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-06-02 11:44:02 -0300

    add: interface to repeatly randomwalks

--- random_walk_visual.py
+++ random_walk_visual.py
@@ -2,8 +2,19 @@ import matplotlib.pyplot as plot
 
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
 
-plot.scatter(rw.x_values, rw.y_values, s=15)
-plot.show()
+# Keep making new walks
+while True:
+    # Make a random walk, and plot the points
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    plot.scatter(rw.x_values, rw.y_values, s=15)
+    plot.show()
+    
+    keep_running = input("Make another walk? (y/n): ")
+    while keep_running != 'y' and keep_running != 'n':
+        keep_running = input("Make another walk? (y/n): ")
+    
+    if keep_running == 'n':
+        break
