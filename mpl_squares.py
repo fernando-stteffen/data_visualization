fe73228a71e0975ecfb75912e7f06b58e8a03baa
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-06-02 08:47:29 -0300

    add: resizing ticks labels then change axis and chart titles

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,5 +1,18 @@
 import matplotlib.pyplot as plot
 
 squares = [1, 4, 9, 16, 25]
-plot.plot(squares)
+plot.plot(squares, linewidth=5)
+
+# Set chart title and label axes.
+plot.title("Square Numbers", fontsize=24)
+plot.xlabel("Value" , fontsize=14)
+plot.ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels.
+plot.tick_params(axis='both', labelsize=14)
+
+
+
 plot.show()
+
+
