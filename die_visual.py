830c92d9886d2a18d3faf3a5b5749db3dbeb72eb
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-06-04 10:24:07 -0300

    add: simulate two d6 rolled 1000 times and export that to svg

--- die_visual.py
+++ die_visual.py
@@ -3,19 +3,21 @@ from die import Die
 
 
 # Create a D6.
-die = Die()
+die_1 = Die()
+die_2 = Die()
 
 
 # Make some rolls, and store results in a list
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
     
     
 # Analyze the results.
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(1, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
@@ -24,11 +26,13 @@ for value in range(1, die.num_sides+1):
 # Visualize the results.
 historic = pygal.Bar()
 
-historic.title = "Results of rolling one D6 1000 times."
-historic.x_labels = ['1','2','3','4','5','6']
+historic.title = "Results of rolling two D6 1000 times."
+labels = list(range(2,13))
+
+historic.x_labels = labels
 historic.x_title = "Result"
 historic.y_title = "Frequency of Result"
 
-historic.add('D6', frequencies)
+historic.add('D6 + D6', frequencies)
 historic.render_to_file('die_visual.svg')
 
