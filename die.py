96af57724ff3dcb873501efb02a888d4a57bc11b
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-06-04 09:54:03 -0300

    add: die class

---
+++ die.py
@@ -0,0 +1,14 @@
+from random import randint
+
+class Die():
+    """A class representing a single die."""
+    
+    
+    def __init(self, num_sides=6):
+        self.num_sides = num_sides
+        
+    def roll(self):
+        """Return a radom value between 1 and number of sides."""
+        return randint(1, self.num_sides)
+
+            
