d62e95f2d8fec51fa4dc526646e857ff761cd7c0
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-06-05 09:38:28 -0300

    add: group level os populations on world_map

--- json_reader/world_population.py
+++ json_reader/world_population.py
@@ -1,4 +1,5 @@
 import pygal
+from pygal.style import LightColorizedStyle, RotateStyle
 import json
 from countries_code import get_country_code
 
@@ -19,11 +20,34 @@ for population in population_data:
         code = get_country_code(country_name)
         if code:
             countrycode_populations[code] = population_total
-        
 
-world_map = pygal.maps.world.World()
+# Group the countries into three populations levels
+
+populations_level_1, populations_level_2, populations_level_3, = {}, {}, {}
+
+for country_code, population in countrycode_populations.items():
+    
+    if population < 10000000:
+        populations_level_1[country_code] = population
+    elif population < 1000000000:
+        populations_level_2[country_code] = population
+    else:
+        populations_level_3[country_code] = population
+        
+print(
+    len(populations_level_1), 
+    len(populations_level_2), 
+    len(populations_level_3)
+)
+
+
+
+world_map_style = RotateStyle('#336699', base_style=LightColorizedStyle)
+world_map = pygal.maps.world.World(style=world_map_style)
 world_map.title = "World population in 2010, by country"
-world_map.add('2010', countrycode_populations)
+world_map.add('0-10m', populations_level_1)
+world_map.add('10m-1b', populations_level_2)
+world_map.add('>1b', populations_level_3)
 
 world_map.render_to_file('world_population.svg')
             
